52b9b11022db5f1f955107e948fa97c5f83adf1b
A.Tomita <warabi0999@gmail.com>
2010-12-13 16:41:14 +0900

    Wall追加Activityのレイアウト設定

--- src/la/yakumo/facebook/tomofumi/TextPostActivity.java
+++ src/la/yakumo/facebook/tomofumi/TextPostActivity.java
@@ -2,13 +2,23 @@ package la.yakumo.facebook.tomofumi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class TextPostActivity
     extends Activity
 {
+    private static final String TAG = Constants.LOG_TAG;
+
     @Override
     public void onCreate(Bundle bndl)
     {
         super.onCreate(bndl);
+        setContentView(R.layout.post_wall);
+    }
+
+    public void onClickSend(View v)
+    {
+        Log.i(TAG, "onClickSend");
     }
 }
